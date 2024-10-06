5c220272a07efe3a7225b65eaa770f24b207e380
walber <walberoc@gmail.com>
2024-10-05 23:20:55 -0300

    isolando os html's em tamplates

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Alura Space!</h1><p>Bem vindo ao espaço</p>")
+    return render(request, 'index.html')
