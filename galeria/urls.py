3b3ee0594c05600cf131423546ea75e21253a26f
walber <walberoc@gmail.com>
2024-10-07 03:33:08 -0300

    inicio estudo de rotas

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
 
 # criar lista para manter todos os endpoints relacionados a nossa galeria
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('imagem/', imagem)
 ]
