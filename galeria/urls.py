4547f1b18cfb72ceece4480e0c6bb7fcf957bcbf
Walber <walberoc@gmail.com>
2024-10-12 21:59:43 -0300

    roteamento de url com python

--- galeria/urls.py
+++ galeria/urls.py
@@ -3,6 +3,6 @@ from galeria.views import index, imagem
 
 # criar lista para manter todos os endpoints relacionados a nossa galeria
 urlpatterns = [
-    path('', index),
-    path('imagem/', imagem)
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem')
 ]
