c26d92e32dc08cf415b1a34738f85b72d51573f6
Walber <walberoc@gmail.com>
2024-10-14 16:42:42 -0300

    pegando imagens dinamicamente

--- galeria/urls.py
+++ galeria/urls.py
@@ -4,5 +4,5 @@ from galeria.views import index, imagem
 # criar lista para manter todos os endpoints relacionados a nossa galeria
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/', imagem, name='imagem')
+    path('imagem/<int:foto_id>', imagem, name='imagem')
 ]
