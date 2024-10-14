4bb40d0d81cc4255a7a236d7d77f1984cb1fca7a
Walber <walberoc@gmail.com>
2024-10-14 10:17:33 -0300

    amplia a importação de configurações de apps

--- galeria/views.py
+++ galeria/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 
+from galeria.models import Fotografia
+
 
 def index(request):
-    dados = {
-    1:{"nome":"Nebulosa de Carina",
-        "legenda":"webtelescope.org / NASA / James Web"},
-    2:{"nome":"galáxia NGC 1079",
-        "legenda":"nasa.org / NASA / Hubble"},
-    }
-    return render(request, 'galeria/index.html', {"cards":dados})
+
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards":fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html') # após, definir ainda a rota
